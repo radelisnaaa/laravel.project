5b88cd0fa64a97b2e7f6afe6f54294dd4e88ef6d
adawiah <r416820@gmail.com>
2025-07-01 19:16:38 +0700

    perbaikan dasbor

--- app/Models/Notification.php
+++
@@ -1,3 +0,0 @@
-<?php
-// Model ini tidak diperlukan, gunakan bawaan Laravel (Illuminate\Notifications\DatabaseNotification)
-// File ini dibiarkan kosong untuk mencegah autoload model custom Notification
